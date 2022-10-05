f2c0447bfd6b9e791734a9e42aafa56ae092411a
tdayve <113845394+tdayve@users.noreply.github.com>
2022-10-05 21:53:49 +0300

    Add files via upload

---
+++ while program.cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,sum=0;
+
+    while(i>=0){
+    sum +=i;
+    cout<<"enter a number"<<endl;
+    cin>>i;}
+    cout<<endl<<"sum = "<<sum<<endl;
+
+
+
+    return 0;
+}
