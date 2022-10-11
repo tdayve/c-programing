0b537e9fb24fe0be82c3affdf05e12e253c41a4f
tdayve <113845394+tdayve@users.noreply.github.com>
2022-10-11 16:44:11 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+float temperature(int fahrenheit);
+float celcius;
+int main()
+{
+    float celcius;
+    float fahrenheit;
+    cout << "enter temperature in fahrenheit" << endl;
+    cin>>fahrenheit;
+    celcius=temperature(fahrenheit);
+    cout << "temperature in celcius = " <<celcius<< endl;
+
+    return 0;
+}float temperature(int fahrenheit){
+float celcius;
+
+celcius=(fahrenheit-33)*0.555556;
+return celcius;
+
+           }
