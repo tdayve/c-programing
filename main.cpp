c25acd8205a7e77e3364ec58c5777b2f636999e0
tdayve <113845394+tdayve@users.noreply.github.com>
2022-11-10 05:43:44 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class cylinder{
+public:
+float r,h,pi=3.142;
+float volume(){
+    return (pi*r*r*h);
+}float surface_area;
+float surface_Area(){
+ surface_area=(2*pi*r*r)+(pi*r*h);
+ return surface_area;
+}
+}int main(){
+    cylinde cylinder1;
+    int a,b,volume,area;
+    a=cylinder1.r=4;
+    b=cylinde1.h=10
+
+
+
+
